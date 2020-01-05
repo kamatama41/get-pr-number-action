8eacf239647b832f8a4186620983d3028f79b9b6
Shinichi Ishimura <sishimura@seqsense.com>
2020-01-05 21:05:07 +0900

    tmp

--- index.js
+++ index.js
@@ -4,30 +4,39 @@ const Octokit = require("@octokit/rest");
 const octokit = new Octokit({auth: core.getInput('github_token')});
 
 async function main() {
-    const event = github.context.payload;
-    const fullName = event['repository']['full_name'];
-    const [owner, repo] = fullName.split('/');
-    const headCommit = event['head_commit']['id'];
     core.debug(`The context info: ${JSON.stringify(github.context, undefined, 2)}`);
+    const eventName = github.context['eventName'];
+    const payload = github.context.payload;
 
-    const {data: pulls} = await octokit.pulls.list({
-        owner: owner,
-        repo: repo,
-        state: 'open',
-    });
+    let prNumber;
+    switch (eventName) {
+        case 'pull_request':
+            prNumber = payload["number"];
+            break;
+        case 'push':
+            const fullName = payload['repository']['full_name'];
+            const [owner, repo] = fullName.split('/');
+            const headCommit = payload['head_commit']['id'];
 
-    const pull = pulls.find(pull => {
-        return pull['head']['sha'] === headCommit
-    });
-    if (!pull) {
-        console.log('Pull request not found');
-        core.setOutput('is_found', false);
-        return
+            const {data: pulls} = await octokit.pulls.list({
+                owner: owner,
+                repo: repo,
+                direction: "asc",
+            });
+
+            const pull = pulls.find(pull => {
+                return pull['head']['sha'] === headCommit
+            });
+
+            if (!pull) {
+                throw new Error('Pull request not found');
+            }
+            prNumber = pull['number'];
+            break;
+        default:
+            throw new Error(`Unsupported event: ${eventName}`);
     }
-
-    core.debug(`The PR info: ${JSON.stringify(pull, undefined, 2)}`);
-    core.setOutput('is_found', true);
-    core.setOutput('number', pull['number']);
+    core.setOutput('number', prNumber);
 }
 
 main().catch(error => core.setFailed(error.message));
